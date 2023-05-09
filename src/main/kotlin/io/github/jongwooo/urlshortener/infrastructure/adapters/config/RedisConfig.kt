5bf129bce5c7f7b55de2465dca308bb236e872b6
jongwooo <jongwooo.han@gmail.com>
2023-05-09 21:22:43 +0900

    feat: Add redis configuration

---
+++ src/main/kotlin/io/github/jongwooo/urlshortener/infrastructure/adapters/config/RedisConfig.kt
@@ -0,0 +1,22 @@
+package io.github.jongwooo.urlshortener.infrastructure.adapters.config
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.redis.connection.RedisConnectionFactory
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
+
+@Configuration
+class RedisConfig {
+
+  @Value("\${spring.data.redis.host}")
+  private lateinit var host: String
+
+  @Value("\${spring.data.redis.port}")
+  private var port: Int = 6379
+
+  @Bean
+  fun redisConnectionFactory(): RedisConnectionFactory {
+    return LettuceConnectionFactory(host, port)
+  }
+}
