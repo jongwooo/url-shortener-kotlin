c47a6ddf01f398eff3bfd5f986585da806bf6d8f
jongwooo <jongwooo.han@gmail.com>
2023-05-12 15:24:54 +0900

    refactor: Use meaningful package names

--- src/main/kotlin/io/github/jongwooo/urlshortener/infrastructure/adapters/configuration/RedisConfiguration.kt
+++ src/main/kotlin/io/github/jongwooo/urlshortener/infrastructure/adapters/configuration/RedisConfiguration.kt
@@ -1,4 +1,4 @@
-package io.github.jongwooo.urlshortener.infrastructure.adapters.config
+package io.github.jongwooo.urlshortener.infrastructure.adapters.configuration
 
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
